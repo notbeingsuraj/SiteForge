40a6b03c40114e8e996baa29576912073c6a2ad8
Suraj Kumar <imysuraj1218@gmail.com>
2026-08-29 12:50:24 +0530

    Add test script for extracting brand DNA from Google Maps business data

---
+++ apps/api/test_brand_dna.js
@@ -0,0 +1,18 @@
+import BusinessDataExtractor from './src/services/BusinessDataExtractor.js';
+import BusinessResearchService from './src/services/BusinessResearchService.js';
+import BrandStrategyService from './src/services/BrandStrategyService.js';
+
+const url = 'https://www.google.com/maps/place/Nilkamal+Homes/@30.9003452,75.85667325,17z/data=!3m1!4b1!4m6!3m5!1s0x390feb5b7b7b7b7b:0x1234567890abcdef!8m2!3d30.9003452!4d75.85667325!16s%2Fg%2F11c5q8v7z';
+
+async function test() {
+  const extractedData = await BusinessDataExtractor.extractFromGoogleMapsUrl(url);
+  const businessData = await BusinessResearchService.extractBusinessIntelligence(extractedData);
+  console.log('Business data identity:', JSON.stringify(businessData.identity, null, 2));
+  console.log('Business data location:', JSON.stringify(businessData.location, null, 2));
+  console.log('Business data contact:', JSON.stringify(businessData.contact, null, 2));
+  
+  const brandDNA = await BrandStrategyService.generateBrandDNA(businessData);
+  console.log('\nBrand DNA full:', JSON.stringify(brandDNA, null, 2));
+}
+
+test().catch(console.error);
