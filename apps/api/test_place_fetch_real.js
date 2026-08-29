fc2244e469216e6a1d7a0ec0aa92114e932622e9
Suraj Kumar <imysuraj1218@gmail.com>
2026-08-29 12:55:57 +0530

    Add script to fetch and log Google Maps business data

---
+++ apps/api/test_place_fetch_real.js
@@ -0,0 +1,11 @@
+import BusinessDataExtractor from './src/services/BusinessDataExtractor.js';
+
+const extractor = BusinessDataExtractor;
+const url = 'https://www.google.com/maps/place/Nilkamal+Homes/@30.9003452,75.85667325,17z/data=!3m1!4b1!4m6!3m5!1s0x390feb5b7b7b7b7b:0x1234567890abcdef!8m2!3d30.9003452!4d75.85667325!16s%2Fg%2F11c5q8v7z';
+const pageData = await extractor.fetchPage(url);
+console.log('Status:', pageData.status);
+console.log('Final URL:', pageData.url);
+console.log('--- HTML length ---');
+console.log(pageData.html.length);
+console.log('--- First 15000 chars ---');
+console.log(pageData.html.substring(0, 15000));
